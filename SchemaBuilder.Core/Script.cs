c13f484e4a6ed0265ba1e8fef5381f38a29e66f8
Lamadelrae <matthew.almeida572@gmail.com>
2022-09-30 15:07:01 -0300

    Implementing script

--- SchemaBuilder.Core/Script.cs
+++ SchemaBuilder.Core/Script.cs
@@ -1,6 +1,25 @@
-﻿namespace SchemaBuilder.Core
+﻿using SchemaBuilder.Core.Interfaces.Operations;
+using SchemaBuilder.Core.Interfaces.Operations.Add;
+using SchemaBuilder.Core.Operations.Add;
+
+namespace SchemaBuilder.Core
 {
     public abstract class Script
     {
+        internal int Id { get; private set; }
+
+        internal Queue<IBaseDataStore> Roots { get; private set; } = new Queue<IBaseDataStore>();
+
+        public Script(int id)
+        {
+            Id = id;
+        }
+
+        public IAddContract Add()
+        {
+            var add = new Add();
+            Roots.Enqueue(add);
+            return add;
+        }
     }
 }
